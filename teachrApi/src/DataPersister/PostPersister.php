4e0b803b7d068259acfcd5b485b8065b339f3529
SJamal08 <sidikjam08@gmail.com>
2021-08-15 12:04:47 +0200

    initialize the react native app

--- teachrApi/src/DataPersister/PostPersister.php
+++ teachrApi/src/DataPersister/PostPersister.php
@@ -3,7 +3,9 @@
 namespace App\DataPersister;
 
 use ApiPlatform\Core\DataPersister\DataPersisterInterface;
+use App\Entity\Statistics;
 use App\Entity\Teachr;
+use App\Repository\StatisticsRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 class PostPersister implements DataPersisterInterface
@@ -25,6 +27,22 @@ class PostPersister implements DataPersisterInterface
 
         $this->em->persist($data);
         $this->em->flush();
+
+        if ($this->em->find(Statistics::class, 1) == null) {
+            $stats = new Statistics(0);
+
+            $stats->setCompteur($stats->getCompteur() + 1);
+
+            $this->em->persist($stats);
+            $this->em->flush();
+        } else {
+            $stats = $this->em->find(Statistics::class, 1);
+
+            $stats->setCompteur($stats->getCompteur() + 1);
+
+            $this->em->persist($stats);
+            $this->em->flush();
+        }
     }
 
     public function remove($data)
